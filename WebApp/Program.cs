c8c5924f5140162ecca91c80b7d38da4f9dd2f3a
Camilo Nossa <calejandro_noss@outlook.com>
2024-10-11 19:49:34 -0500

    🔧 Add default connection string

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,7 +1,39 @@
+using Microsoft.EntityFrameworkCore;
+using WebApp.Models.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Get environment variables
+var dbServer = Environment.GetEnvironmentVariable("DB_SERVER") ?? "localhost";
+var dbDatabase = Environment.GetEnvironmentVariable("DB_DATABASE") ?? "MyDb";
+var dbUser = Environment.GetEnvironmentVariable("DB_USER") ?? "sa";
+var dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD") ?? "Password123!";
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    var defaultConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    
+    System.Console.WriteLine("Is defaultConnectionString null or empty?");
+    System.Console.WriteLine(string.IsNullOrEmpty(defaultConnectionString));
+
+    if (string.IsNullOrEmpty(defaultConnectionString))
+    {
+        throw new InvalidOperationException("Connection string not found.");
+    }
+    
+    defaultConnectionString = defaultConnectionString
+        .Replace("${DB_SERVER}", dbServer)
+        .Replace("${DB_DATABASE}", dbDatabase)
+        .Replace("${DB_USER}", dbUser)
+        .Replace("${DB_PASSWORD}", dbPassword);
+    
+    System.Console.WriteLine($"Connection string: {defaultConnectionString}");
+
+    options.UseSqlServer(defaultConnectionString);
+});
 
 var app = builder.Build();
 
